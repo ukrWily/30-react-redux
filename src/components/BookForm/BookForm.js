c77cc06d03c940a41a9e4399d11c56dfc6f0f848
Vitalii Spartesnyi <ukrwily@gmail.com>
2024-09-09 20:30:26 +0300

    PC BookForm

--- src/components/BookForm/BookForm.js
+++ src/components/BookForm/BookForm.js
@@ -1,7 +1,35 @@
+import { useState } from "react";
+//
 const BookForm = () => {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  //
+  const handleSubmit = () => {
+    // dispatch action
+  };
   return (
     <div className="app-block book-form">
       <h2>Add a New Book</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="title">Title</label>
+          <input
+            type="text"
+            id="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="author">Author</label>
+          <input
+            type="text"
+            id="author"
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+          />
+        </div>
+      </form>
     </div>
   );
 };
---
+++ src/components/Filter/Filter.js
@@ -0,0 +1,8 @@
+const Filter = () => {
+  return (
+    <div className="app-block filter">
+      <h2>Filters</h2>
+    </div>
+  );
+};
+export default Filter;
