9aadacd140d3b78c495f33e7ad3e0dcefd4a3102
Vitalii Spartesnyi <ukrwily@gmail.com>
2024-09-24 19:43:25 +0300

    filter

---
+++ src/redux/slices/filterSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  title: "",
+};
+
+const filterSlice = createSlice({
+  name: "filter",
+  initialState,
+  reducers: {
+    setTitleFilter: (state, action) => {
+      return { ...state, title: action.payload };
+    },
+  },
+});
+
+console.log(filterSlice.actions);
+export default filterSlice.reducer;
